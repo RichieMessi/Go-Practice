6ab2690b7b2fd686ddcab02343e7aeb07d87f566
Richardson <Messirichards@gmail.com>
2017-10-22 03:58:03 -0400

    Oct_22

---
+++ duck.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Fuck you")
+
+	fmt.Println("60 + 10 =", 6+10)
+	fmt.Println("60 - 10 =", 60-10)
+	fmt.Println("60 / 10 =", 60/10)
+	fmt.Println("60 * 10 =", 60*10)
+	fmt.Println("60 % 10 =", 60%10)
+
+	name := "Naruto"
+	age := 16
+	attack := "Rasin Shuriken"
+	village := "Village hidden in the leaf"
+
+	fmt.Println("My name is", name, "and my age is", age, "and my village is", village, "and my attack is", attack)
+
+	var num int = 100
+	var num_1 int = 200
+	fmt.Println(num_1 - num)
+
+	var foo uint64 = 89234
+	fmt.Println(foo)
+
+}
