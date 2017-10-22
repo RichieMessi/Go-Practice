0e610bfa2e3302dfe8702e86d5409d4718067a7e
Richardson <Messirichards@gmail.com>
2017-10-22 03:58:35 -0400

    oct_22 part_2

---
+++ duck2.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const pi float64 = 3.14159265
+	fmt.Println(pi)
+
+	// var (
+	// 	foo = 10
+	// 	boo = 20
+	// )
+
+	var myName = "Richie Pie"
+	fmt.Println(myName)
+	fmt.Println(len(myName), "is the number of characters in 'myName'")
+
+	fmt.Println(myName + " is a leaf shinobi.. I am applying at Anbu Black Ops as part time job")
+	fmt.Println(myName, " \n   \n \n   \n")
+	fmt.Println(myName, "New Lines at the top brooo... we killin it")
+
+	// var lightsOn bool = false
+
+	fmt.Printf("%f \n", pi)
+
+}
